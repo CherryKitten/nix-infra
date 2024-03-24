b15138f104142bb30303dbe4e3d33271748bd8ad
CherryKitten <sammy@cherrykitten.dev>
2024-03-24 19:47:29 +0100

    fix home-manager in nixos config

--- hosts/common/users.nix
+++ hosts/common/users.nix
@@ -17,7 +17,7 @@ in
       openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJZyQSZw+pExsx2RXB+yxbaJGB9mtvudbQ/BP7E1yKvr openpgp:0x6068FEBB" ];
     };
 
-    home-manager.users.sammy = lib.mkIf cfg.sammy.enable (import ../../users/sammy.nix)
-      // lib.mkIf (lib.pathExists (../../users + "/sammy@${hostname}.nix")) (import ../../users + "sammy@${hostname}.nix");
+    home-manager.users.sammy = lib.mkIf cfg.sammy.enable (import ../../users/sammy.nix);
+# something with this is wrong lol      // lib.mkIf (lib.pathExists (../../users + "/sammy@${hostname}.nix")) (import ../../users + "sammy@${hostname}.nix");
   };
 }
