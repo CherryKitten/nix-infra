cf626fef856abcba25b1e51802fc46777217275d
CherryKitten <sammy@cherrykitten.dev>
2024-05-05 10:56:59 +0200

    more sway changes

--- profiles/desktop/sway/default.nix
+++ profiles/desktop/sway/default.nix
@@ -34,7 +34,9 @@
     services.swayidle =
       let
         lockCommand = "${pkgs.writeShellScript "swaylock-command" ''
-        ${pkgs.swaylock}/bin/swaylock
+        ${pkgs.grim}/bin/grim -t png -l 1 /tmp/lock-screenshot.png
+        ${pkgs.imagemagick}/bin/magick /tmp/lock-screenshot.png -blur 80x40 /tmp/lock-screenshot.png
+        ${pkgs.swaylock}/bin/swaylock -i /tmp/lock-screenshot.png -efFl
       ''}";
       in
       {
@@ -50,7 +52,6 @@
     wayland.windowManager.sway =
       let
         cfg = config.home-manager.users.sammy.wayland.windowManager.sway;
-        modifier = "Mod4";
       in
       {
         enable = true;
@@ -59,7 +60,7 @@
         config = {
           fonts = {
             names = [ "JetBrains Mono" ];
-            size = 8.0;
+            size = 9.0;
           };
           terminal = "foot";
           menu = "wofi --show drun";
@@ -128,72 +129,63 @@
             };
           }];
 
+          modifier = "Mod4";
           keybindings = {
-            "${modifier}+Return" = "exec ${cfg.config.terminal}";
+            "${cfg.config.modifier}+Return" = "exec ${cfg.config.terminal}";
+            "${cfg.config.modifier}+d" = "exec ${cfg.config.menu}";
 
-            "${modifier}+Left" = "focus left";
-            "${modifier}+Down" = "focus down";
-            "${modifier}+Up" = "focus up";
-            "${modifier}+Right" = "focus right";
+            "${cfg.config.modifier}+l" = "exec loginctl lock-session";
 
-            "${modifier}+Shift+Left" = "move left";
-            "${modifier}+Shift+Down" = "move down";
-            "${modifier}+Shift+Up" = "move up";
-            "${modifier}+Shift+Right" = "move right";
+            "${cfg.config.modifier}+Shift+e" =
+              "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -b 'Yes, exit sway' 'swaymsg exit'";
+            "${cfg.config.modifier}+Shift+q" = "kill";
 
-            "${modifier}+h" = "split h";
-            "${modifier}+v" = "split v";
+            "${cfg.config.modifier}+Shift+s" = "exec ${pkgs.grim}/bin/grim -t png -l 1 -g \"$(${pkgs.slurp}/bin/slurp)\" ~/Pictures/screenshot-$(date +%Y-%m-%d_%H-%m-%s).png";
 
-            "${modifier}+s" = "layout stacked";
-            "${modifier}+w" = "layout tabbed";
+            "${cfg.config.modifier}+Left" = "focus left";
+            "${cfg.config.modifier}+Down" = "focus down";
+            "${cfg.config.modifier}+Up" = "focus up";
+            "${cfg.config.modifier}+Right" = "focus right";
 
-            "${modifier}+Shift+space" = "floating toggle";
-            "${modifier}+space" = "focus mode_toggle";
-            "${modifier}+a" = "focus parent";
+            "${cfg.config.modifier}+Shift+Left" = "move left";
+            "${cfg.config.modifier}+Shift+Down" = "move down";
+            "${cfg.config.modifier}+Shift+Up" = "move up";
+            "${cfg.config.modifier}+Shift+Right" = "move right";
 
-            "${modifier}+f" = "fullscreen toggle";
+            "${cfg.config.modifier}+b" = "split h";
+            "${cfg.config.modifier}+v" = "split v";
 
-            "${modifier}+1" = "workspace 1";
-            "${modifier}+2" = "workspace 2";
-            "${modifier}+3" = "workspace 3";
-            "${modifier}+4" = "workspace 4";
-            "${modifier}+5" = "workspace 5";
-            "${modifier}+6" = "workspace 6";
-            "${modifier}+7" = "workspace 7";
-            "${modifier}+8" = "workspace 8";
-            "${modifier}+9" = "workspace 9";
-            "${modifier}+0" = "workspace 10";
-            "${modifier}+F1" = "workspace 11";
-            "${modifier}+F2" = "workspace 12";
-            "${modifier}+F3" = "workspace 13";
-            "${modifier}+F4" = "workspace 14";
-            "${modifier}+F5" = "workspace 15";
-            "${modifier}+F6" = "workspace 16";
-            "${modifier}+F7" = "workspace 17";
-            "${modifier}+F8" = "workspace 18";
-            "${modifier}+F9" = "workspace 19";
-            "${modifier}+F10" = "workspace 20";
+            "${cfg.config.modifier}+s" = "layout stacking";
+            "${cfg.config.modifier}+w" = "layout tabbed";
+            "${cfg.config.modifier}+e" = "layout toggle split";
 
-            "${modifier}+Shift+1" = "move container to workspace 1";
-            "${modifier}+Shift+2" = "move container to workspace 2";
-            "${modifier}+Shift+3" = "move container to workspace 3";
-            "${modifier}+Shift+4" = "move container to workspace 4";
-            "${modifier}+Shift+5" = "move container to workspace 5";
-            "${modifier}+Shift+6" = "move container to workspace 6";
-            "${modifier}+Shift+7" = "move container to workspace 7";
-            "${modifier}+Shift+8" = "move container to workspace 8";
-            "${modifier}+Shift+9" = "move container to workspace 9";
-            "${modifier}+Shift+0" = "move container to workspace 10";
-            "${modifier}+Shift+F1" = "move container to workspace 11";
-            "${modifier}+Shift+F2" = "move container to workspace 12";
-            "${modifier}+Shift+F3" = "move container to workspace 13";
-            "${modifier}+Shift+F4" = "move container to workspace 14";
-            "${modifier}+Shift+F5" = "move container to workspace 15";
-            "${modifier}+Shift+F6" = "move container to workspace 16";
-            "${modifier}+Shift+F7" = "move container to workspace 17";
-            "${modifier}+Shift+F8" = "move container to workspace 18";
-            "${modifier}+Shift+F9" = "move container to workspace 19";
-            "${modifier}+Shift+F10" = "move container to workspace 20";
+            "${cfg.config.modifier}+Shift+space" = "floating toggle";
+            "${cfg.config.modifier}+space" = "focus mode_toggle";
+            "${cfg.config.modifier}+a" = "focus parent";
+
+            "${cfg.config.modifier}+f" = "fullscreen toggle";
+
+            "${cfg.config.modifier}+1" = "workspace 1";
+            "${cfg.config.modifier}+2" = "workspace 2";
+            "${cfg.config.modifier}+3" = "workspace 3";
+            "${cfg.config.modifier}+4" = "workspace 4";
+            "${cfg.config.modifier}+5" = "workspace 5";
+            "${cfg.config.modifier}+6" = "workspace 6";
+            "${cfg.config.modifier}+7" = "workspace 7";
+            "${cfg.config.modifier}+8" = "workspace 8";
+            "${cfg.config.modifier}+9" = "workspace 9";
+            "${cfg.config.modifier}+0" = "workspace 10";
+
+            "${cfg.config.modifier}+Shift+1" = "move container to workspace 1";
+            "${cfg.config.modifier}+Shift+2" = "move container to workspace 2";
+            "${cfg.config.modifier}+Shift+3" = "move container to workspace 3";
+            "${cfg.config.modifier}+Shift+4" = "move container to workspace 4";
+            "${cfg.config.modifier}+Shift+5" = "move container to workspace 5";
+            "${cfg.config.modifier}+Shift+6" = "move container to workspace 6";
+            "${cfg.config.modifier}+Shift+7" = "move container to workspace 7";
+            "${cfg.config.modifier}+Shift+8" = "move container to workspace 8";
+            "${cfg.config.modifier}+Shift+9" = "move container to workspace 9";
+            "${cfg.config.modifier}+Shift+0" = "move container to workspace 10";
 
             "XF86AudioRaiseVolume" = "exec --no-startup-id ${pkgs.pamixer}/bin/pamixer -i 5";
             "XF86AudioLowerVolume" = "exec --no-startup-id ${pkgs.pamixer}/bin/pamixer -d 5";
@@ -202,26 +194,14 @@
             "XF86MonBrightnessDown" = "exec ${pkgs.light}/bin/light -U 5";
             "XF86MonBrightnessUp" = "exec ${pkgs.light}/bin/light -A 5";
 
-            "${modifier}+l" = "exec loginctl lock-session";
-            "${modifier}+d" = "exec ${cfg.config.menu}";
-
-            "${modifier}+Shift+c" = "reload";
-            "${modifier}+Shift+e" = "exit";
-            "${modifier}+Shift+q" = "kill";
-
-            "${modifier}+r" = "mode resize";
-
-            "${modifier}+Shift+s" = "exec ${pkgs.grim}/bin/grim -t png -l 1 -g \"$(${pkgs.slurp}/bin/slurp)\" ~/screenshot-$(date +%Y-%m-%d_%H-%m-%s).png";
+            "${cfg.config.modifier}+r" = "mode resize";
           };
-
         };
 
         extraConfig = ''
           client.focused #00000000 #000000cc #FFFFFF
           client.unfocused #00000000 #00000070 #FFFFFF
           client.focused_inactive #00000000 #00000090 #FFFFFF
-          titlebar_border_thickness 3
-          titlebar_padding 8 6
         '';
       };
   };
