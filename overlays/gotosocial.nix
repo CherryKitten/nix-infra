a01fbaaf5f75325d7f34a7faa77335044a9d07b5
CherryKitten <sammy@cherrykitten.dev>
2024-07-03 11:52:05 +0200

    update gts

--- overlays/gotosocial.nix
+++ overlays/gotosocial.nix
@@ -1,10 +1,10 @@
 final: prev: {
   gotosocial =
     let
-      version = "0.16.0-rc3";
+      version = "0.16.0";
       web-assets = prev.fetchurl {
         url = "https://github.com/superseriousbusiness/gotosocial/releases/download/v${version}/gotosocial_${version}_web-assets.tar.gz";
-        hash = "sha256-HlpctwO58owAbPc3BJpid1LP8B956L7HqWI10iy3jOU=";
+        hash = "sha256-aZQpd5KvoZvXEMVzGbWrtGsc+P1JStjZ6U5mX6q7Vb0=";
       };
     in
     prev.gotosocial.overrideAttrs {
@@ -14,7 +14,7 @@ final: prev: {
         owner = "superseriousbusiness";
         repo = "gotosocial";
         rev = "refs/tags/v${ version }";
-        hash = "sha256-jmVOMuBaojoCVCXsvcfDSsj+Oy0gosk2QQr2Paa5qL0=";
+        hash = "sha256-QoG09+jmq5e5vxDVtkhY35098W/9B1HsYTuUnz43LV4=";
       };
       postInstall = ''
         tar xf ${web-assets}
