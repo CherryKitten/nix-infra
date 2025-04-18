8cf2c8230b9f0720409e4af40814cdfc67bb3773
CherryKitten <sammy@cherrykitten.dev>
2025-04-18 14:31:24 +0200

    update gotosocial

--- overlays/gotosocial.nix
+++ overlays/gotosocial.nix
@@ -1,10 +1,10 @@
 final: prev: {
   gotosocial =
     let
-      version = "0.18.2";
+      version = "0.19.0-rc1";
       web-assets = prev.fetchurl {
         url = "https://github.com/superseriousbusiness/gotosocial/releases/download/v${version}/gotosocial_${version}_web-assets.tar.gz";
-        hash = "sha256-36UwUhf3FZ+/DMI0L/g88prbEwyj2ApoRdMK8f57KCU=";
+        hash = "sha256-JgV55NR8xKKIxWl1mNuKIwmWnHOXD5e5WxprhcEk8GM=";
       };
     in
     prev.gotosocial.overrideAttrs {
@@ -14,12 +14,18 @@ final: prev: {
         owner = "superseriousbusiness";
         repo = "gotosocial";
         rev = "refs/tags/v${ version }";
-        hash = "sha256-GHUHtTE8KQtm+sWr5K+WmOr3KY7gA9hDINIBTioXNlw=";
+        hash = "sha256-pncGwZJY1R5rbxUhRim79lXLqqb/lbAwAchLifKj2/A=";
       };
       postInstall = ''
         tar xf ${web-assets}
         mkdir -p $out/share/gotosocial
         mv web $out/share/gotosocial/
       '';
+      ldflags = [
+        "-s"
+        "-w"
+        "-X main.Version=${version}"
+      ];
+
     };
 }
